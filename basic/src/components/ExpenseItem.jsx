5e9a03e7a1e468c9e520a25ef7c66bd8ca382c57
sungmin <95615086+Clariatinet@users.noreply.github.com>
2023-07-20 21:58:44 +0900

    ㅁㅁ

--- basic/src/components/ExpenseItem.jsx
+++ basic/src/components/ExpenseItem.jsx
@@ -12,6 +12,8 @@ const ExpenseItem = () => {
         <h2>{expenseTitle}</h2>
         <h2>{expenseTitle}</h2>
         <div className='expense-item__price'>{expenseAmount}</div>
+        <div className='expense-item__price'>{expenseAmount}</div>
+        <div className='expense-item__price'>{expenseAmount}</div>
       </div>
     </div>
   );
